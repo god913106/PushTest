e18074abe470adb80cb08a9754d3f99acec20852
楊崇平 <god913106@gmail.com>
2018-08-10 18:01:19 +0800

    設定CSR file 去keyChain設定憑證  PushTest Profile Dev 在swift上寫取得token的code  模擬器中不支持遠程通知

--- PushTest/AppDelegate.swift
+++ PushTest/AppDelegate.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 import Firebase
+import UserNotificationsUI
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate{
 
     var window: UIWindow?
 
@@ -19,6 +21,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
+        
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: {_, _ in })
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        
+        if let token = InstanceID.instanceID().token() {
+            print("偷肯Token: \(token)")
+        }
         
         return true
     }
